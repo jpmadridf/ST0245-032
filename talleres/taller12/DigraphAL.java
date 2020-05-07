f9ecdcf6ec3886e6f6e1f4572858f944ab6abc01
jpmadridf <57046870+jpmadridf@users.noreply.github.com>
2020-05-06 19:32:27 -0500

    Add files via upload

---
+++ talleres/taller12/DigraphAL.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import javafx.util.Pair;
+
+public class DigraphAL extends Graph {
+
+    ArrayList<LinkedList<Pair<Integer,Integer>>> nodo;
+
+    public DigraphAL(int size) {
+        super(size);
+        nodo = new ArrayList<>();
+        for (int i = 0; i < size + 1; i++) {
+            nodo.add(new LinkedList<>() );
+
+        }
+    }
+
+    public void addArc(int source, int destination, int weight) {
+        nodo.get(source).add(new Pair<>(destination,weight));
+    }
+
+    public ArrayList<Integer> getSuccessors(int vertex) {
+        ArrayList<Integer> n = new ArrayList<>(); 
+        nodo.get(vertex).forEach(i -> n.add(i.getValue()));
+        return n;
+    }
+
+    public int getWeight(int source, int destination) {
+        int result = 0;
+        for (Pair<Integer, Integer> integerIntegerPair : nodo.get(source)) {
+            if (integerIntegerPair.getKey() == source ) result = integerIntegerPair.getValue();
+        }
+        return result;
+    }
+}
+
